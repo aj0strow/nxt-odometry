e6bc644e268ff68d8722b35790fca187c8e44ac5
aj0strow <alexander.ostrow@gmail.com>
2013-09-21 14:25:33 -0400

    fixed to Lab2

--- compile.sh
+++ compile.sh
@@ -1,5 +1,5 @@
 source clean.sh;
 echo -n "compiling project... ";
 find src -type f -name *.java | xargs nxjc -d classes;
-cd classes && nxjlink -o ../Lab1.nxj Lab1;
+cd classes && nxjlink -o ../Lab2.nxj Lab2;
 cd .. && echo "complete!"
